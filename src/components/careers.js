5df47f35f93b0006f05ed107cf4467dd6353728e
Adam Pearson <adam.pearson.1996@gmail.com>
2020-03-06 11:08:38 +1100

    Update careers.js

--- src/components/careers.js
+++ src/components/careers.js
@@ -1,44 +1,46 @@
 import React from "react";
 import { Link } from "gatsby";
-import "../scss/careers.scss";
+import "../../scss/careers.scss";
 
-const Careers = () => {
-  return (
-    <section className="section careers-section">
-      <div className="container">
-        <h5 className="section-header">Join our team</h5>
-        <div className="column-container">
-          <div className="column">
-            <p>
-              <Link to={this.props.link1} className="" title="">
-                {this.props.linktitle1}
-              </Link>
-              {this.props.description1}
-            </p>
+class Careers extends React.Component {
+  render() {
+    return (
+      <section className="section careers-section">
+        <div className="container">
+          <h5 className="section-header">Join our team</h5>
+          <div className="column-container">
+            <div className="column">
+              <p>
+                <Link to={this.props.link1} className="" title="">
+                  {this.props.linktitle1}
+                </Link>
+                {this.props.description1}
+              </p>
+            </div>
+            <div className="column">
+              <p>
+                <Link to={this.props.link2} className="" title="">
+                  {this.props.linktitle2}
+                </Link>
+                {this.props.description2}
+              </p>
+            </div>
+            <div className="column">
+              <p>
+                <Link to={this.props.link3} className="" title="">
+                  {this.props.linktitle3}
+                </Link>
+                {this.props.description3}
+              </p>
+            </div>
           </div>
-          <div className="column">
-            <p>
-              <Link to={this.props.link2} className="" title="">
-                {this.props.linktitle2}
-              </Link>
-              {this.props.description2}
-            </p>
-          </div>
-          <div className="column">
-            <p>
-              <Link to={this.props.link3} className="" title="">
-                {this.props.linktitle3}
-              </Link>
-              {this.props.description3}
-            </p>
-          </div>
+          <Link to="/careers" className="no-decoration line-link" title="">
+            View all careers
+          </Link>
         </div>
-        <Link to="/careers" className="no-decoration line-link" title="">
-          View all careers
-        </Link>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default Careers;
