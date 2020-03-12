04641df6afb6e18c31c164fa791a9a263dbead95
Joel Brewster <hi@joelbrewster.com>
2020-03-12 13:13:58 +1100

    formatting

--- src/components/audits.js
+++ src/components/audits.js
@@ -6,32 +6,36 @@ class Audits extends React.Component {
   render() {
     return (
       <section className="section audits-section">
-          <div className="outer-container">
-            <div className="container-max-width">
-              <div className="inner-container">
-          <h5 className="section-header">Our audits</h5>
-          <div className="column-container">
-            <div className="column">
-              <Link to="/digital-media-audit" className="" title="">
-                Digital Media <span>Audit</span>
-              </Link>
-              <p>Audit me, bruz</p>
-            </div>
-            <div className="column">
-              <Link to="/organic-search-audit" className="" title="">
-                Search Engine Optimisation <span>Audit</span>
-              </Link>
-              <p>Page one, or die trying</p>
-            </div>
-          <div className="column">
-              <Link to="/conversion-rate-optimisation-audit" className="" title="">
-                Conversion Rate Optimisation <span>Audit</span>
-              </Link>
-              <p>Get more bang for your buck</p>
+        <div className="outer-container">
+          <div className="container-max-width">
+            <div className="inner-container">
+              <h5 className="section-header">Our audits</h5>
+              <div className="column-container">
+                <div className="column">
+                  <Link to="/digital-media-audit" className="" title="">
+                    Digital Media <span>Audit</span>
+                  </Link>
+                  <p>Audit me, bruz</p>
+                </div>
+                <div className="column">
+                  <Link to="/organic-search-audit" className="" title="">
+                    Search Engine Optimisation <span>Audit</span>
+                  </Link>
+                  <p>Page one, or die trying</p>
+                </div>
+                <div className="column">
+                  <Link
+                    to="/conversion-rate-optimisation-audit"
+                    className=""
+                    title=""
+                  >
+                    Conversion Rate Optimisation <span>Audit</span>
+                  </Link>
+                  <p>Get more bang for your buck</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        </div>
         </div>
       </section>
     )
